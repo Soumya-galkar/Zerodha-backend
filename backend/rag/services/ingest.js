f5441a33d377539802597007c22b88ac8224ab3b
SoumyaGalkar <soumyagalkar@gmail.com>
2026-08-18 03:08:24 +0530

    Update ingest.js

--- backend/rag/services/ingest.js
+++ backend/rag/services/ingest.js
@@ -1,29 +1,48 @@
+
 const extractTextFromPDF = require("../parser/pdfParser");
 const { splitText } = require("../chunking/chunker");
 const { getEmbedding } = require("../embeddings/embedder");
-const { addDocument } = require("../vectorDB/faiss");
+const { saveChunk } = require("../../services/supabaseVector");
 
 async function ingestDocument(filePath) {
+
     console.log("Extracting PDF...");
+
     const text = await extractTextFromPDF(filePath);
 
     console.log("Chunking...");
+
     const chunks = await splitText(text);
 
     console.log(`Chunks: ${chunks.length}`);
 
     console.log("Generating embeddings...");
 
-    for (const chunk of chunks) {
-        const embedding = await getEmbedding(chunk.pageContent);
+    // Get filename from path
+    const path = require("path");
+    const fileName = path.basename(filePath);
 
-        addDocument(
-            embedding,
+    for (const chunk of chunks) {
+
+        console.log("Generating embedding for chunk...");
+
+        const embedding = await getEmbedding(
             chunk.pageContent
+        );
+
+        console.log(
+            "Embedding length:",
+            embedding.length
+        );
+
+        await saveChunk(
+            fileName,
+            chunk.pageContent,
+            embedding
         );
     }
 
-    console.log("Document Indexed Successfully");
+    console.log("Document saved to Supabase successfully");
 
     return chunks.length;
 }
